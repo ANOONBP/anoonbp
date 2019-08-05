6e5372af8b9eb69219bd3bbd89c0fec6364714d6
ANOONBP <0Protocal0@gmail.com>
2019-08-05 20:12:43 +0700

    Car atm beta

---
+++ precache-manifest.b71b2c24fa02ecf3b5c5917cb1a6d12c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a6897047cd2341c0de66380faff610",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e11bdcb5be3b2caf3ef",
+    "url": "/static/css/main.ee53a54e.chunk.css"
+  },
+  {
+    "revision": "c178504deec153525d86",
+    "url": "/static/js/2.7b150a5b.chunk.js"
+  },
+  {
+    "revision": "0e11bdcb5be3b2caf3ef",
+    "url": "/static/js/main.5a15242f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "306f3510840ac6b2031b9d35e2302c52",
+    "url": "/static/media/about_us.306f3510.svg"
+  },
+  {
+    "revision": "5658d4a59c3a1eadda874cb896f65f8c",
+    "url": "/static/media/body_01.5658d4a5.svg"
+  },
+  {
+    "revision": "9153907ddeaa9e3d885c350dd89f2073",
+    "url": "/static/media/body_02.9153907d.svg"
+  },
+  {
+    "revision": "5fb00352a00dd23e21d490c29ebfa60d",
+    "url": "/static/media/body_03.5fb00352.svg"
+  },
+  {
+    "revision": "57f07520c7d6cec1fb5e3b60fbbfa330",
+    "url": "/static/media/facebook.57f07520.svg"
+  },
+  {
+    "revision": "318a1ce5fcdb0fe7a38ff5176d8ff54b",
+    "url": "/static/media/header.318a1ce5.svg"
+  },
+  {
+    "revision": "911e480d0a989bdf9a75c3102b21ca7e",
+    "url": "/static/media/line.911e480d.svg"
+  },
+  {
+    "revision": "407addfd332d9bb4f2d5f20b43a15375",
+    "url": "/static/media/line_icon.407addfd.svg"
+  },
+  {
+    "revision": "55a43ea93702ffd0f1b990904fa8a8ec",
+    "url": "/static/media/logo.55a43ea9.svg"
+  },
+  {
+    "revision": "b1fb1f776cb42d2017ff94bc38162437",
+    "url": "/static/media/warning_icon.b1fb1f77.svg"
+  }
+]);
